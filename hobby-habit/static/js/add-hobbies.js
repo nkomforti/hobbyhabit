be2a1a95964741dcd8b4f1c2bedb229b1df13917
Natalie Komforti <dorcas2dorcas@gmail.com>
2017-11-10 21:07:43 +0000

    Modified add-hobbies.js, add-hobbies.html @WIP.

--- hobby-habit/static/js/add-hobbies.js
+++ hobby-habit/static/js/add-hobbies.js
@@ -1,29 +1,59 @@
-// Create counting variable.
-// let numHobbies = 1;
 
-// Attach event listener to element with id plus-btn.
+// Select element with id plus-button and attach an event listener to it that
+// will call the following anonymous function when clicked.
 $("#plus-btn").click(function (evt) {
 
+    // Get the vlaue for the input with the id numHobbies and bind to variable.
+    let numHobbies = $("#num-hobbies").val();
+    // Convert string to integer.
+    numHobbies = parseInt(numHobbies);
     // Increment numHobbies.
-    // get the vlaue for the input with the id numHobbies.
-    let numHobbies = $("#num-hobbies").val();  // $("#num-hobbies").val(numHubbies);
-    numHobbies = parseInt(numHobbies)
     numHobbies += 1;
 
-    // Create new add hobby field everytime the plus-btn is clicked.
-    // let newHobbyNameField = $(`<input type='text' id='hobby-name-${numHobbies}' name='hobby-name-${numHobbies}'>`);  //using string literals to update field with each iteration.
+    // Every time the plus button is clicked, create new add hobby input field.
     let newHobbyNameField = $("<input>");
-
-    // Add attributes to the new field.
+    // Add attributes to the new input field.
     newHobbyNameField.attr({"id": "hobby-name-" + numHobbies,
                             "type": "text",
                             "name": "hobby-name-" + numHobbies,
-                            "placeholder": "Hobby No." + numHobbies,
+                            "placeholder": "Hobby No. " + numHobbies,
+                            "display": "block"
     });
 
     // Put the new input into the DOM.
     $("#add-hobbies").append(newHobbyNameField);
-    // numHobbies += 1;
-    $("#num-hobbies").val(numHobbies)
+
+    // Get the value for the input witht the id num-hobbies.
+    $("#num-hobbies").val(numHobbies);
+    // Disable the properties for the element with the id plus-btn.
+    $("#plus-btn").prop("disabled", true);
 });
 
+// Select entire page/document and attach an event listenter to it that will
+// call the following anonymous function when a key on the keyboard is released.
+$(document).on("keyup", "#add-hobbies input", function (evt) {
+
+        // Create variable to track input field values; set to false to start.
+        let empty = false;
+
+        // Select all elements with the id add-hobbies that are also input
+        // elements, and run method to iterate over the collection of elements
+        // and will call the following function. 
+        $("#add-hobbies input").each(function (evt){
+
+            // Check if the input value of the current loop iteration (object
+            // that currently "owns" the function) is equal to an empty string.
+            if ($(this).val() === "" ){
+                // If value equals empty string, disable plus button, and set
+                // tracking variable equal to true (because field is empty).
+                $("#plus-btn").prop("disabled", true);
+                empty = true;
+            }   
+        });
+
+        // Check if tracking variable equals false (input fields not empty).
+        // If false enable plus button.
+        if (empty === false) {
+            $("#plus-btn").prop("disabled", false);
+        }
+    });
