772d1ffd93fd29516598cf53453cab231f4bac2d
Natalie Komforti <dorcas2dorcas@gmail.com>
2017-11-21 00:12:44 +0000

    Dashboard @WIP.

--- hobby-habit/static/js/dashboard.js
+++ hobby-habit/static/js/dashboard.js
@@ -1,34 +1,34 @@
 "use strict";
 
 $(document).ready(function(){
-    $("#user-profile > p").hide();
+    $("#user-profile > #user-profile-content").hide();
     $("#my-hobbyhabits > #my-hobbyhabit-content").show();
     $("#social > p").hide();
     $("#settings > p").hide();
 
     $("#user-profile-menu-btn").click(function(evt) {
-        $("#user-profile > p").show();
+        $("#user-profile > #user-profile-content").show();
         $("#my-hobbyhabits > #my-hobbyhabit-content").hide();
         $("#social > p").hide();
         $("#settings > p").hide();
     });
 
     $("#my-hobbyhabits-menu-btn").click(function(evt) {
-        $("#user-profile > p").hide();
+        $("#user-profile > #user-profile-content").hide();
         $("#my-hobbyhabits > #my-hobbyhabit-content").show();
         $("#social > p").hide();
         $("#settings > p").hide();
     });
 
     $("#social-menu-btn").click(function(evt) {
-        $("#user-profile > p").hide();
+        $("#user-profile > #user-profile-content").hide();
         $("#my-hobbyhabits > #my-hobbyhabit-content").hide();
         $("#social > p").show();
         $("#settings > p").hide();
     });
 
     $("#settings-menu-btn").click(function(evt) {
-        $("#user-profile > p").hide();
+        $("#user-profile > #user-profile-content").hide();
         $("#my-hobbyhabits > #my-hobbyhabit-content").hide();
         $("#social > p").hide();
         $("#settings > p").show();
